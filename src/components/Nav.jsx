88afd9fb5373b38515ad94ce3e4f63564283a8e6
Sarthakagrawal213 <sarthakagrawal213@gmail.com>
2026-01-21 23:55:58 +0530

    Fix image paths for GitHub Page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -23,14 +23,14 @@ const Nav = () => {
   return (
     <div id='nav'>
       <div id="left">
-         <img className='apple' src="/Apple_logo_white.svg.png" alt="" />
+         <img className='apple' src="/portfolio1/Apple_logo_white.svg.png" alt="" />
            <p >Sarthak Agrawal</p>
           <p>File</p>
           <p>Window</p>
           <p>Terminal</p> 
       </div>
       <div id="right">
-        <img className='wifi' src="/wireless-xxl.png" alt="" />
+        <img className='wifi' src="/portfolio1/wireless-xxl.png" alt="" />
         <div className="clock" aria-live="polite">{formatNow(now)}</div>
       </div>
     </div>
